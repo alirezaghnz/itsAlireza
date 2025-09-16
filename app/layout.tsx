808976a1bd6abf7b94e90c0ba71b6718667d1c25
alirezaghnzd <changlee76301@gmail.com>
2025-09-16 22:37:55 +0330

    Add: NextTopLoader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import GlobalLoading from "./_components/globalLoading";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Alireza ghnz",
@@ -16,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
-      <body className="bg-gray-900">{children}</body>
+      <body className="bg-gray-900">
+        <NextTopLoader color="#2563eb" showSpinner={true} />
+        {children}
+      </body>
     </html>
   );
 }
